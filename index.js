ecc536b280e8b72db81b03133b9234923b6a3489
Ahmad Saifullah Arifin <asaifulas@gmail.com>
2022-12-04 00:06:04 +0800

    add connection to top post and search

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const axios = require('axios');
 
 let app = express()
 app.use(express.json())
+
+// Frontpage
 app.get('/', (req, res)=>{
-    res.send('test1')
+    res.send('Back end Test for Ahmad Saifullah Arifin')
+})
+
+// First task, get top-post 
+app.post('/top-post', (req, res)=>{
+
+})
+
+// Second task, search query
+app.get('/search', (req, res)=>{
+
 })
 
 app.listen(3002)
